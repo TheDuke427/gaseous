797eb1adcc112543db99c5aaabead763ef4e183f
TheDuke427 <56452519+TheDuke427@users.noreply.github.com>
2025-11-15 07:00:17 -0800

    Update run.sh

--- gaseous_server/run.sh
+++ gaseous_server/run.sh
@@ -3,7 +3,6 @@
 # Home Assistant Add-on: Gaseous Server
 # ==============================================================================
 
-# Log startup
 bashio::log.info "Starting Gaseous Server add-on..."
 
 # Configuration variables
@@ -15,14 +14,12 @@ IGDB_CLIENT_ID=$(bashio::config 'igdb_client_id')
 IGDB_CLIENT_SECRET=$(bashio::config 'igdb_client_secret')
 PORT=$(bashio::config 'port')
 
-# Optional: Wait for DB to be ready
 bashio::log.info "Waiting for database to be ready..."
 until mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" -e "USE $DB_NAME;" &>/dev/null; do
     sleep 2
     bashio::log.info "Still waiting..."
 done
 
-# Start Gaseous Server
 bashio::log.info "Launching Gaseous Server..."
 exec java -jar /opt/gaseous/gaseous-server.jar \
     --database.host="$DB_HOST" \
