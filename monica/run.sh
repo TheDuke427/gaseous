86451596e8b54b230211ee9fe0883230d40c2c27
TheDuke427 <56452519+TheDuke427@users.noreply.github.com>
2025-11-16 13:46:45 -0800

    Update run.sh

--- monica/run.sh
+++ monica/run.sh
@@ -38,8 +38,8 @@ mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASSWORD" "$DB_DATABASE" -e 
 php83 artisan config:cache
 php83 artisan route:cache
 
-echo "Starting PHP-FPM on port 9000..."
-php-fpm83 -F -R --listen 127.0.0.1:9000 &
+echo "Starting PHP-FPM on 127.0.0.1:9000..."
+php-fpm83 -F -R -y /dev/null -d listen=127.0.0.1:9000 -d pm=dynamic -d pm.max_children=5 -d pm.start_servers=2 -d pm.min_spare_servers=1 -d pm.max_spare_servers=3 &
 
 sleep 2
 
