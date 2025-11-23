170f5e06b372fdc7597960cd069d06ce6ca237b3
TheDuke427 <56452519+TheDuke427@users.noreply.github.com>
2025-11-23 09:03:16 -0800

    Update run.sh

--- audiobookshelf/run.sh
+++ audiobookshelf/run.sh
@@ -10,6 +10,15 @@ bashio::log.info "Config directory: ${CONFIG_PATH}"
 bashio::log.info "Metadata directory: ${METADATA_PATH}"
 bashio::log.info "Port: ${PORT}"
 
-# Start Audiobookshelf
-cd /app
-exec node index.js
+# Find where audiobookshelf is installed and run it
+# The official image should have the app somewhere, we'll find and exec it
+if [ -f /usr/local/bin/audiobookshelf ]; then
+    exec /usr/local/bin/audiobookshelf
+elif [ -f /app/index.js ]; then
+    cd /app && exec node index.js
+elif [ -f /server/index.js ]; then
+    cd /server && exec node index.js  
+else
+    bashio::log.error "Cannot find audiobookshelf executable!"
+    exit 1
+fi
