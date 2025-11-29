13ce97d3684f8546016a6ee9eee5ee06d65e13f0
TheDuke427 <56452519+TheDuke427@users.noreply.github.com>
2025-11-29 09:45:15 -0800

    Update run.sh

--- puter/run.sh
+++ puter/run.sh
@@ -5,8 +5,8 @@ echo "[Puter] Starting..."
 
 cd /opt/puter
 
-# Let Puter accept all hosts and bind to all interfaces
-export NODE_ENV=production
-export PUTER_HOST_OVERRIDE=0.0.0.0
+# Allow any host (necessary inside HA add-on)
+export PUTER_DEV_ALLOW_ANY_HOST=1
 
-exec npm start -- --port 4100 --host 0.0.0.0 --allow-any-host
+# Bind to all interfaces
+exec npm start -- --port 4100 --host 0.0.0.0
