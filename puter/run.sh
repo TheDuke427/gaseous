21dc51b49665f992640474ae5d779dc195847f34
TheDuke427 <56452519+TheDuke427@users.noreply.github.com>
2025-11-29 09:25:37 -0800

    Update run.sh

--- puter/run.sh
+++ puter/run.sh
@@ -5,5 +5,5 @@ echo "[Puter] Starting..."
 
 cd /opt/puter
 
-# Don’t daemonize. Don’t background. Let HA supervise.
+# Don't daemonize, don't background. Let HA supervise.
 exec npm start -- --port 4100
