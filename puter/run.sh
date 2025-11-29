8de5da298dbe66a0b6b0131660ef44fcf461fe85
TheDuke427 <56452519+TheDuke427@users.noreply.github.com>
2025-11-29 08:12:31 -0800

    Update run.sh

--- puter/run.sh
+++ puter/run.sh
@@ -5,12 +5,7 @@ echo "[INFO] Starting Puter..."
 
 export PUTER_CONFIG_PATH="/data/config"
 export PUTER_DATA_PATH="/data/data"
-export PUID=1000
-export PGID=1000
-
-mkdir -p /data/config /data/data
 
 cd /app
 
-echo "[INFO] Launching Puter..."
 exec npm start
