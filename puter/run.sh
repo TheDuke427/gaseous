ceb037a94c6c598c78a842621c4c41499b1c6118
TheDuke427 <56452519+TheDuke427@users.noreply.github.com>
2025-11-29 07:55:01 -0800

    Update run.sh

--- puter/run.sh
+++ puter/run.sh
@@ -1,16 +1,15 @@
-#!/command/with-contenv bashio
+#!/bin/bash
+set -e
+
+echo "[INFO] Starting Puter..."
 
 CONFIG_PATH=/data/options.json
-
 DOMAIN=$(jq -r '.domain // "puter.localhost"' $CONFIG_PATH)
 
-# Set environment variables
 export PUTER_CONFIG_PATH="/data/config"
 export PUTER_DATA_PATH="/data/data"
 
-# Navigate to app directory
 cd /app
 
-# Start Puter
-bashio::log.info "Starting Puter on domain: $DOMAIN..."
+echo "[INFO] Launching on domain: $DOMAIN"
 exec npm start
