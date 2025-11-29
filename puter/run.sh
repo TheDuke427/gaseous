4d224723fd1b96d582273588ed5cfa7ba82fe56e
TheDuke427 <56452519+TheDuke427@users.noreply.github.com>
2025-11-29 09:50:07 -0800

    Delete puter directory

--- puter/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "[Puter] Starting in production mode..."
-
-cd /opt/puter
-
-# Run the production server, bind to all interfaces
-exec npm run serve -- --port 4100 --host 0.0.0.0
